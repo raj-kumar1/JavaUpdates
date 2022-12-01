9802d2f691c1ffc84fad006e05c35384ed4ef71c
raj-kumar1 <raj.kumar@catalystone.com>
2022-12-01 15:29:24 +0100

    pusing class for switch example

---
+++ src/main/java/jdk19to12/switchexpression/DemoSwitch.java
@@ -0,0 +1,7 @@
+package jdk19to12.switchexpression;
+
+public class DemoSwitch {
+    public static void main(String[] args) {
+        System.out.println("Hello World here are some examples of switch Expression!!");
+    }
+}
